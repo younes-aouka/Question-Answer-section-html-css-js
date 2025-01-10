aec6c19f0eee94b39191b2372f962b4594e41ad9
Younes-aouka <youneseaouka@gmail.com>
2025-01-10 16:55:30 +0100

    enhancment

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-const questions = document.querySelectorAll(".question");
-console.log(questions);
+const Buttons = document.querySelectorAll(".container .question .question-section .icon");
 
-questions.forEach(function(question){
-    question.addEventListener("click",function(){
+Buttons.forEach(function(button){
+    button.addEventListener("click",function(){
     try{
-        question.querySelector(".plus").classList.toggle("hide-plus");
-        question.querySelector(".minus").classList.toggle("show-minus");
-        question.querySelector(".answer-section").classList.toggle("show-answer");
+        button.querySelector(".plus").classList.toggle("hide-plus");
+        button.querySelector(".minus").classList.toggle("show-minus");
+        button.parentElement.nextElementSibling.classList.toggle("show-answer");
     }catch(e){
         console.log("Error occured! \n",e);
     }
